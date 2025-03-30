41ce11432d340986c605a18367e3c50f05edbb7a
Tanishka Verma <tanishkaverma43@gmail.com>
2025-03-30 22:40:30 +0530

    link changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['https://e-commer-frontend.vercel.app'],
 }));
 
 // Database Connection With MongoDB
