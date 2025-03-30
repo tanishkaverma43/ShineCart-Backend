a2650fdd2f9a8deb9f3dd96d4f34c738e0b9ad14
Tanishka Verma <tanishkaverma43@gmail.com>
2025-03-30 22:34:31 +0530

    some minor changes

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const cors = require("cors");
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors({origin: "https://e-commer-frontend.vercel.app",
-       methods: "GET,POST,PUT,PATCH,DELETE",
-       credentials: true,
-       allowedHeaders: "Content-Type, Autherization"
-       
+app.use(cors({
+  origin: ['http://localhost:3000'],
 }));
 
 // Database Connection With MongoDB
